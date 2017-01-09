4bbe03840c140e38da73838ac36889086d0596de
Elizabeth Davis <eadskye@gmail.com>
2017-01-09 13:39:14 -0700

    added form

--- client/demo-form/demo-form.component.js
+++ client/demo-form/demo-form.component.js
@@ -4,6 +4,16 @@
   angular
   .module("app", [])
   .component("demoForm", {
-    templateUrl: "demo-form/demo-form.template.html"
+    templateUrl: "demo-form/demo-form.template.html",
+    controller: DemoFormController
   });
+  function_DemoFormController() {
+    var vm= this;
+    vm.submitUsername = submitUsername;
+    vm.user= {};
+    function submitUsername(username){
+      console.log(username);
+    }
+
+  }
 })();
